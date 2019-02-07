e00005d44f1e6a9f17b9eece02ad116653f77671
Snaxai <Daniel-mossestad@hotmail.com>
2019-02-07 14:38:19 +0100

    oppgave 3 gjort

--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,16 @@ func SumInt8(a, b int8) int8 {
 func SumFloat64(a, b float64) float64 {
 	return a + b
 }
+
+func SumInt32(a, b int32) int32{
+	return a + b
+}
+
+func SumuInt32(a, b uint32) uint32{
+        return a + b
+}
+
+func SumInt64(a, b int64) int64{
+        return a + b
+}
+
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,20 +3,80 @@ package sum
 import "testing"
 
 // Check https://golang.org/ref/spec#Numeric_types and stress the limits!
-var sum_tests_int8 = []struct {
-	n1       int8
-	n2       int8
-	expected int8
+
+var sum_tests_int64 = []struct {
+        n1       int64
+        n2       int64
+        expected int64
 }{
-	{1, 2, 3},
-	{4, 5, 9},
-	{120, 1, 119},
+        {2222, 4444, 6666},
+        {4, 5, 9},
+        {120, -1, 119},
 }
 
-func TestSumInt8(t *testing.T) {
-	for _, v := range sum_tests_int8 {
-		if val := SumInt8(v.n1, v.n2); val != v.expected {
-			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
-		}
-	}
+func TestSumInt64(t *testing.T) {
+        for _, v := range sum_tests_int64 {
+                if val := SumInt64(v.n1, v.n2); val != v.expected {
+                        t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+                }
+        }
 }
+
+
+var sum_tests_float64 = []struct {
+        n1       float64
+        n2       float64
+        expected float64
+}{
+        {2.1, 2.3, 4.4},
+        {4.1, 5.1, 9.2},
+        {120.1, 1.1, 121.2},
+}
+
+func TestSumFloat64(t *testing.T) {
+        for _, v := range sum_tests_float64 {
+                if val := SumFloat64(v.n1, v.n2); val != v.expected {
+                        t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+                }
+        }
+}
+
+
+var sum_tests_uint32 = []struct {
+        n1       uint32
+        n2       uint32
+        expected uint32
+}{
+        {222, 444, 666},
+        {4, 5, 9},
+        {120, 1, 121},
+}
+
+func TestSumuInt32(t *testing.T) {
+        for _, v := range sum_tests_uint32 {
+                if val := SumuInt32(v.n1, v.n2); val != v.expected {
+                        t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+                }
+        }
+}
+
+var sum_tests_int32 = []struct {
+        n1       int32
+        n2       int32
+        expected int32
+}{
+        {2222, 4444, 6666},
+        {4, 5, 9},
+        {120, -1, 119},
+}
+
+
+
+func TestSumInt32(t *testing.T) {
+        for _, v := range sum_tests_int32 {
+                if val := SumInt32(v.n1, v.n2); val != v.expected {
+                        t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+                }
+        }
+}
+
